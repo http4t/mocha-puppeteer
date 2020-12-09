786bc2d2b15e93d8c997e2a9cf3ab483fd82655e
Matt Savage <matthew.savage@gmail.com>
2020-12-09 20:51:18 +0000

    Fixing build

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sed -i '' "s/\"version\": \"1.0.0\"/\"version\": \"1.0.$GITHUB_RUN_NUMBER\"/g" package.json
+sed -i '' "s/\(\"version\": \"[0-9]*\.[0-9]*\.\)[0-9]*\(.*\)/\1$GITHUB_RUN_NUMBER\2/g" package.json
