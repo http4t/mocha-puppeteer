c9b3dace7c25543e4bc6722746fe246fdc58c8bc
Matt Savage <matthew.savage@gmail.com>
2022-06-28 18:50:05 +0100

    Upgrading everything and making timeout configurable

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@
 'use strict'
 import fs from "fs";
 import * as path from "path";
-import Puppeteer, {Browser} from "puppeteer";
+import Puppeteer, {Browser, BrowserConnectOptions, BrowserLaunchArgumentOptions, LaunchOptions, Page} from "puppeteer";
+import {ChildProcess} from "child_process";
 
 const {spawn} = require('child_process');
 const {TextDecoder} = require('util');
@@ -52,18 +53,23 @@ if (importMocha.length !== 0) {
     throw new Error(`Importing 'describe' or 'it' from mocha in test files breaks browser testing in ${importMocha.join(", ")}`);
 }
 
-function getExtraOpts(): object {
+function getExtraOpts(): LaunchOptions & BrowserLaunchArgumentOptions & BrowserConnectOptions {
     if (!process.env.PUPPETEER_EXEC_PATH) return {};
     console.log(`PUPPETEER_EXEC_PATH=${process.env.PUPPETEER_EXEC_PATH}`)
     return {executablePath: process.env.PUPPETEER_EXEC_PATH};
 }
 
-async function run() {
-    const parcel = spawn("parcel", ["serve", "mocha.html"]);
-    parcel.stdout.pipe(process.stdout);
-    parcel.stderr.pipe(process.stderr);
+async function launchPuppeteerBrowser(extraOpts: object) {
+    return await Puppeteer.launch.bind(Puppeteer)(Object.assign(
+        {
+            headless: true,
+            args: ['--no-sandbox']
+        },
+        extraOpts));
+}
 
-    const parcelStarted = new Promise((resolve, reject) => {
+function parcelServerRunning(parcel: ChildProcess) {
+    return new Promise((resolve, reject) => {
         parcel.on("exit", (code: number | null) => {
             if (code == 0) {
                 resolve(code);
@@ -71,46 +77,68 @@ async function run() {
                 reject(code);
             }
         })
-        parcel.stdout.on('data', (data: Buffer) => {
-            const text = decoder.decode(data)
+        // noinspection TypeScriptValidateTypes
+        parcel.stdout?.on("data", (chunk: any):void => {
+            const text = decoder.decode(chunk)
             if (text.includes("Server running at ")) {
                 resolve(null)
             }
         });
     });
-    const extraOpts = getExtraOpts();
-    const browser: Browser = await Puppeteer.launch.bind(Puppeteer)(Object.assign(
-        {
-            headless: true,
-            args: ['--no-sandbox']
-        },
-        extraOpts));
+}
+
+function pipePageConsoleToProcConsole(page: Page) {
+    page.on("console", (message: any) => {
+        (async () => {
+            const args = await Promise.all(message.args().map((a: any) => a.jsonValue()));
+            const log = (console as any)[message.type()];
+            typeof log === 'function'
+                ? log(...args)
+                : console.log(...args);
+        })();
+    });
+}
+
+async function openPage(browser: Browser) {
+    const page = await browser.newPage();
+    pipePageConsoleToProcConsole(page);
+    return page;
+}
+
+async function runMochaTests(page: Page, parcelServerUrl: string) {
+    if(process.env.PUPPETEER_LOAD_TIMEOUT_MILLIS) console.log("Using PUPPETEER_LOAD_TIMEOUT_MILLIS="+process.env.PUPPETEER_LOAD_TIMEOUT_MILLIS)
+    let puppeteerTimeout = process.env.PUPPETEER_LOAD_TIMEOUT_MILLIS ? parseInt(process.env.PUPPETEER_LOAD_TIMEOUT_MILLIS) : 20000;
+
+    await page.goto(parcelServerUrl, {waitUntil: 'load', timeout: puppeteerTimeout});
+
+    await page.evaluate(() => {
+        return new Promise((resolved: Function, rejected: Function) => {
+            // @ts-ignore
+            mocha
+                .reporter('spec')
+                .run((failures: any) => failures == 0
+                    ? resolved("SUCCESS")
+                    : rejected("FAILED: " + failures))
+        });
+    });
+}
+
+async function run() {
+    const parcel = spawn("parcel", ["serve", "mocha.html"]);
+    parcel.stdout.pipe(process.stdout);
+    parcel.stderr.pipe(process.stderr);
+
+    const parcelServerPromise = parcelServerRunning(parcel);
+    const browser: Browser = await launchPuppeteerBrowser(getExtraOpts());
 
     try {
-        const page = await browser.newPage();
-        page.on("console", (message: any) => {
-            (async () => {
-                const args = await Promise.all(message.args().map((a: any) => a.jsonValue()));
-                const log = (console as any)[message.type()];
-                typeof log === 'function'
-                    ? log(...args)
-                    : console.log(...args);
-            })();
-        });
+        // We open page in puppeteer and have parcel compiling in parallel...
+        const page = await openPage(browser);
 
-        await parcelStarted;
-        await page.goto("http://localhost:1234", {waitUntil: 'load', timeout: 20000});
+        // ...and then wait for parcel to catch up
+        await parcelServerPromise;
 
-        await page.evaluate(() => {
-            return new Promise((resolved: Function, rejected: Function) => {
-                // @ts-ignore
-                mocha
-                    .reporter('spec')
-                    .run((failures: any) => failures == 0
-                        ? resolved("SUCCESS")
-                        : rejected("FAILED: " + failures))
-            });
-        });
+        await runMochaTests(page, "http://localhost:1234");
     } finally {
         await browser.close();
         parcel.kill();
